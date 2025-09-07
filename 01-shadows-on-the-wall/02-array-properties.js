64c61722878164bb9bea14ec4ab0cab2d70a66b2
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2025-09-07 22:45:07 +0300

    unify format

--- 01-shadows-on-the-wall/02-array-properties.js
+++ 01-shadows-on-the-wall/02-array-properties.js
@@ -1,12 +1,12 @@
 // ...you stare at the flickers on the screen.
 
 const a = [];
-a.x = 3;
-a.y = 3;
+a["x"] = 3;
+a["y"] = 3;
 
 
 
 
 const b = [];
-b[0] = 3;
-b[1] = 3;
+b["0"] = 3;
+b["1"] = 3;
--- 01-shadows-on-the-wall/03-array-like-objects.js
+++ 01-shadows-on-the-wall/03-array-like-objects.js
@@ -1,12 +1,13 @@
 // Is it the real world that you are looking at?
 
-const a = {
-  length: 0,
-};
-a[0] = 3;
-a[1] = 3;
+const a = { length: 0 };
+a["0"] = 3;
+a["1"] = 3;
 a.length = 2;
 
+
+
 const b = [];
-b[0] = 3;
-b[1] = 3;
+b["0"] = 3;
+b["1"] = 3;
+a.length = 2;
